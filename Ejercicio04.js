7ee8fa3f14d681510dbd90d76e39aaef68f0505e
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-10-30 21:33:43 -0500

    Finalizado Ejercicio 4

---
+++ Ejercicio04.js
@@ -0,0 +1,13 @@
+function mayorDeTres(a,b,c) {
+    let mayor=0;
+    if (a>mayor){
+        mayor=a;
+    }
+    if (b>mayor){
+        mayor=b;
+    } 
+    if (c>mayor){
+        mayor=c; 
+    }
+    return `${mayor} es el número mayor`; 
+}
