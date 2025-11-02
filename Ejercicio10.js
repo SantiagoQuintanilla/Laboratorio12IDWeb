fe41cdf1dcf57ad26b3bdfab0c38972ca32b0475
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-11-02 17:08:48 -0500

    Finalizado Ejercicio 10

--- Ejercicio10.js
+++ Ejercicio10.js
@@ -1,7 +1,6 @@
 const func1=texto=>texto.toUpperCase();
 const func2=texto=>`${texto}!`;
-const componerTransformaciones = function(func1, func2){
-    let texto=``;
-    const func3 = texto => func2(func1(texto));
-    return func3(texto);
-}
+
+const componerTransformaciones = (f1, f2) => texto => f2(f1(texto));
+
+const transformar=componerTransformaciones(func1, func2);
